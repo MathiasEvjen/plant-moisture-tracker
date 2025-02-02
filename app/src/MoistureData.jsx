bef31d216ff759f90e44c14ab64b6cef55cda096
Mathias-Evjen <143395521+Mathias-Evjen@users.noreply.github.com>
2025-02-02 23:56:24 +0100

    Ryddet opp i filstruktur og fjernet unødvendig fil

---
+++ app/src/MoistureData.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+const MoistureData = () => {
+    const [moistureData, setMoistureData] = useState("");
+    const ESPIP = "http://192.168.4.30/data";
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(ESPIP);
+                console.log(response);
+                const data = await response.json();
+                console.log(data.text());
+                setMoistureData(data.value);
+                console.log("Hentet data: " + data.value);
+            } catch (error) {
+                console.error("Feil ved henting av data: " + error);
+                setMoistureData("Feil ved henting");
+            }
+        };
+
+        const interval = setInterval(fetchData, 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div>
+            <h1>Moisture Data:</h1>
+            <h2>{moistureData}%</h2>
+        </div>
+    );
+}
+
+export default MoistureData;
